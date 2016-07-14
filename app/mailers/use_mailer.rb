b5377b99ca24af5b3493c7331f0bd3ff41ba04e9
Chad Fennell <fenne035@umn.edu>
2016-07-14 15:27:31 -0500

    don't spam jason

--- app/mailers/use_mailer.rb
+++ app/mailers/use_mailer.rb
@@ -1,8 +1,8 @@
 class UseMailer < ActionMailer::Base
-  default(:from => "jronallo@gmail.com")
+  default(:from => "fenne035@umn.edu")
   def use_email(upload)
     @upload = upload
-    mail(:to => "jronallo@gmail.com", :subject => "[steady] #{@upload.name} #{@upload.organization}")
+    mail(:to => "fenne035@umn.edu", :subject => "[steady] #{@upload.name} #{@upload.organization}")
     attachments['upload.csv'] = @upload.csv.read if @upload.csv
   end
 end
